d280d3320dabb66402232d8f478c13695c81ba22
Pulkit Gupta <pulgupta@gmail.com>
2019-04-01 12:09:12 +0530

    updated

--- src/com/jaxrs/rest/controllers/UserController.java
+++ src/com/jaxrs/rest/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.jaxrs.rest.controllers;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/UserService")
@@ -26,4 +27,12 @@ public class UserController {
 	   student.setName("Pulkit");
 	   return student;
    }	
+   @GET
+   @Path("/usersquery")
+   @Produces(MediaType.APPLICATION_JSON)
+   public Student getUsers1(@QueryParam(value = "Pulkit") String studentName){
+	   Student student = new Student();
+	   student.setName(studentName);
+	   return student;
+   }
 }
